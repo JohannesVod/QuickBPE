70bcd4d6bc214f65a0b55c0fec5605e9f15b1863
voderhoj00 <voderhoj00@zedat.fu-berlin.de>
2024-11-29 16:46:05 +0100

    working??

--- minbpe/fastfuncs/funcsLin.c
+++ minbpe/fastfuncs/funcsLin.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 typedef struct Node {
     int v;
     int index;
     struct Node *l;
     struct Node *r;
-    struct Node *next_occ;
     struct Node *prev_occ;
+    struct Node *next_occ;
     struct Pair *pair;
 } Node;
 
@@ -20,10 +21,10 @@ typedef struct {
 typedef struct Pair {
     int a;
     int b;
+    int num_occurrences;
     // first occurrence:
     Node *first_occurrence;
     Node *last_occurrence;
-    int num_occurrences;
     // points to next/prev pair, has same number of occurrences (later used for heap):
     struct Pair *next;
     struct Pair *prev;  
@@ -34,6 +35,7 @@ typedef struct {
 } Token;
 
 void printL(LinkedList *L) {
+    printf("List: \n");
     Node *curr = L->start;
     int i=0;
     while (curr != NULL) {
@@ -55,7 +57,22 @@ void printL(LinkedList *L) {
         }
         if (curr->next_occ != NULL){
             if (curr->next_occ->prev_occ != curr){
-                printf("|!!!|");
+                if (curr->next_occ->prev_occ == NULL){
+                    printf("|!!(NULL)<-!!|!!->(%d)!!|", curr->next_occ->v);
+                }
+                else{
+                    printf("|!!(%d)<-!!|!!->(%d)!!|", curr->next_occ->prev_occ->v, curr->next_occ->v);
+                }
+            }
+        }
+        if (curr->prev_occ != NULL){
+            if (curr->prev_occ->next_occ != curr){
+                if (curr->prev_occ->next_occ == NULL){
+                    printf("|!?!(NULL)<-!?!|!?!->(%d)!?!|", curr->prev_occ->v);
+                }
+                else{
+                    printf("|!?!(%d)<-!!|!!->(%d)!?!|", curr->prev_occ->next_occ->v, curr->prev_occ->v);
+                }
             }
         }
         if (curr->r != NULL){
@@ -73,16 +90,18 @@ void printL(LinkedList *L) {
 }
 
 void printPair(Pair *p){
-    if (p->num_occurrences == 0){
-        return;
-    }
-    printf("Pair: (%d, %d) Occurrences(%d): \n", p->a, p->b, p->num_occurrences);
+    printf("(%d, %d) (occs: %d): ", p->a, p->b, p->num_occurrences);
     Node *curr = p->first_occurrence;
     while (curr != NULL) {
-        printf("(pos: %d)", curr->index);
+        printf("%d", curr->index);
         if (curr->next_occ != NULL){
             if (curr->next_occ->prev_occ != curr) {
-                printf("!!!!");
+                if (curr->next_occ->prev_occ == NULL){
+                    printf("!!(NULL)<-!!");
+                }
+                else{
+                    printf("!!(%d)<-!!", curr->next_occ->prev_occ->v);
+                }
             }
             else {
                 printf("=");
@@ -91,6 +110,10 @@ void printPair(Pair *p){
         curr = curr->next_occ;
     }
     printf("\n");
+}
+
+void printNode(Node *node){
+    printf("Node: (ind: %d, val: %d)", node->index, node->v);
 }
 
 void printHeap(Pair **h, int heapsize){
@@ -103,7 +126,11 @@ void printHeap(Pair **h, int heapsize){
                 printf("|pair: (%d, %d)|", curr->a, curr->b);
                 if (curr->next != NULL){
                     if (curr->next->prev != curr){
-                        printf("!!!!");
+                        if (curr->next->prev == NULL){
+                            printf("!!((NULL)<-)!!");
+                        } else{
+                            printf("!!((%d, %d)<-)!!", curr->next->prev->a, curr->next->prev->b);
+                        }
                     }
                     else{
                         printf("=");
@@ -114,69 +141,110 @@ void printHeap(Pair **h, int heapsize){
             printf("\n");
         }
     }
+    // print every pair:
+    printf("Pairs: \n");
+    for (int i=0; i<heapsize; i++){
+        if (h[i] != NULL){
+            Pair *curr = h[i];
+            while (curr != NULL){
+                printPair(curr);
+                curr = curr->next;
+            }
+        }
+    }
+    printf("\n");
+}
+
+void removeFromList(LinkedList *l, Node *node){
+    if (node->l != NULL){
+        node->l->r = node->r;
+    }
+    else {
+        l->start = node; // make first if necessary: 
+    }
+    if (node->r != NULL)
+        node->r->l = node->l;
 }
 
 /* Adds pair to heap at correct slot */
-void addToHeap(Pair **heap, Pair *p){
+void addToHeap(Pair **h, Pair *p){
+    //printf("ADD (%d, %d) %d:\n",p->a, p->b, p->num_occurrences);
     int index = p->num_occurrences;
     if (index > 0){
-        if (heap[index] == NULL){
-            heap[index] = p;
+        if (h[index] == NULL){
+            h[index] = p;
         }
         else{
-            p->next = heap[index];
-            heap[index]->prev = p;
-            heap[index] = p;
+            p->next = h[index];
+            h[index]->prev = p;
+            h[index] = p;
         }
     }
 }
 
 /* removes pair from its slot in heap. Assumes the pair is present. */
-void removeFromHeap(Pair **heap, Pair *pair){
-    if (pair->prev != NULL){
-        pair->prev->next = pair->next;
+void removeFromHeap(Pair **h, Pair *p){
+    //printf("REM (%d, %d) %d:\n",p->a, p->b, p->num_occurrences);
+    if (p->prev != NULL){
+        p->prev->next = p->next;
     } else {
         // make first in heap:
-        heap[pair->num_occurrences] = pair->next;
+        h[p->num_occurrences] = p->next;
     }
-    if (pair->next != NULL)
-        pair->next->prev = pair->prev;
+    if (p->next != NULL)
+        p->next->prev = p->prev;
+    p->next = NULL;
+    p->prev = NULL;
+}
+
+void removeOccFromPair(Pair *p, Node *node){
+    // remove occurrence from linked list:
+    if (node->next_occ != NULL)
+        node->next_occ->prev_occ = node->prev_occ;
+    if (node->prev_occ != NULL)
+        node->prev_occ->next_occ = node->next_occ;
+    // make first and last if necessary
+    if (p->first_occurrence == node)
+        p->first_occurrence = node->next_occ;
+    if (p->last_occurrence == node)
+        p->last_occurrence = node->prev_occ;
+    node->prev_occ = NULL;
+    node->next_occ = NULL;
+    node->pair = NULL;
+    p->num_occurrences--;
 }
 
 /* removes occurrence from list and adjusts the the heap accordingly */
 void removeOcc(Pair **heap, Node *node){
     if (node != NULL && node->pair != NULL){
         Pair *this_pair = node->pair;
-        // remove occurrence from linked list:
-        if (node->next_occ != NULL)
-            node->next_occ->prev_occ = node->prev_occ;
-        if (node->prev_occ != NULL)
-            node->prev_occ->next_occ = node->next_occ;
-        // make first and last if necessary
-        if (this_pair->first_occurrence == node)
-            this_pair->first_occurrence = this_pair->first_occurrence->next_occ;
-        if (this_pair->last_occurrence == node)
-            this_pair->last_occurrence = this_pair->last_occurrence->prev_occ;
         // move down the heap:
         removeFromHeap(heap, this_pair);
-        this_pair->num_occurrences--;
+        removeOccFromPair(this_pair, node);
         addToHeap(heap, this_pair);
     }
 }
 
-void addOcc(Pair **h, Pair *p, Node *node){
-    removeFromHeap(h, p);
+void addOcc2Pair(Pair *p, Node *node){
     node->pair = p;
     p->num_occurrences++;
     if (p->first_occurrence == NULL){
         p->first_occurrence = node;
         p->last_occurrence = node;
+        // node->prev_occ = NULL; // does not work like that because start/end node not updated maybe
+        // node->next_occ = NULL;
     }
     else{
         p->last_occurrence->next_occ = node;
         node->prev_occ = p->last_occurrence;
+        // node->next_occ = NULL;
         p->last_occurrence = node;
     }
+}
+
+void addOcc(Pair **h, Pair *p, Node *node){
+    removeFromHeap(h, p);
+    addOcc2Pair(p, node);
     addToHeap(h, p);
 }
 
@@ -187,45 +255,32 @@ typedef struct {
     Pair* ref_r;
 } HashEntry;
 
-Pair *getPairFromHash(HashEntry *hashes, int a, int b, int new_letter){
-    Pair *res;
+Pair *getPairFromHash(Pair **heap, HashEntry *hashes, int a, int b, int new_letter){
     if (b == new_letter){
-        HashEntry this_entry = hashes[a];
-        if (this_entry.prev_connector_l != new_letter){
-            this_entry.prev_connector_l = new_letter;
-            // create new pair:
-            Pair new_p;
-            new_p.num_occurrences = 0;
-            new_p.a = a;
-            new_p.b = b;
-            new_p.first_occurrence = NULL;
-            new_p.last_occurrence = NULL;
-            new_p.next = NULL;
-            new_p.prev = NULL;
-            this_entry.ref_l = &new_p;
+        HashEntry *this_entry = &hashes[a];
+        if (this_entry->prev_connector_l != new_letter){
+            //printf("\nNew!!!(%d, %d) (prev conn: %d)\n", a, b, this_entry->prev_connector_l);
+            this_entry->prev_connector_l = new_letter;
+            Pair *new_p = malloc(sizeof(Pair));
+            *new_p = (Pair) {a, b, 0, NULL, NULL, NULL, NULL};
+            this_entry->ref_l = new_p;
+            addToHeap(heap, new_p);
         } 
-        res = this_entry.ref_l;
+        return this_entry->ref_l;
     }
     else{
-        HashEntry this_entry = hashes[b];
-        if (this_entry.prev_connector_r != new_letter){
-            this_entry.prev_connector_r = new_letter;
-            // create new pair:
-            Pair new_p;
-            new_p.num_occurrences = 0;
-            new_p.a = a;
-            new_p.b = b;
-            new_p.first_occurrence = NULL;
-            new_p.last_occurrence = NULL;
-            new_p.next = NULL;
-            new_p.prev = NULL;
-            this_entry.ref_r = &new_p;
+        HashEntry *this_entry = &hashes[b];
+        if (this_entry->prev_connector_r != new_letter){
+            //printf("\nNew!!!(%d, %d) (prev conn: %d)\n", a, b, this_entry->prev_connector_r);
+            this_entry->prev_connector_r = new_letter;
+            Pair *new_p = malloc(sizeof(Pair));
+            *new_p = (Pair) {a, b, 0, NULL, NULL, NULL, NULL};
+            this_entry->ref_r = new_p;
+            addToHeap(heap, new_p);
         }
-        res = this_entry.ref_r;
+        return this_entry->ref_r;
     }
-    return res;
 }
-
 
 Token* train(int *ids, int n, int k, int init_tokens) {
     Node *nodeMalloc = malloc(sizeof(Node) * n);
@@ -239,45 +294,24 @@ Token* train(int *ids, int n, int k, int init_tokens) {
     for (int i=0; i<init_tokens; i++){
         for (int j=0; j<init_tokens; j++){
             int index = i*init_tokens+j;
-            init_counts[index].num_occurrences = 0;
-            init_counts[index].a = i;
-            init_counts[index].b = j;
-            init_counts[index].first_occurrence = NULL;
-            init_counts[index].last_occurrence = NULL;
-            init_counts[index].next = NULL;
-            init_counts[index].prev = NULL;
+            init_counts[index] = (Pair) {i, j, 0, NULL, NULL, NULL, NULL};
         }
     }
 
     // count initial pairs and build linked list:
-    int prev_a = -1;
-    int prev_b = -1;
-    int prev_added = -1;
     for (int i = 0; i < n; i++) {
         // create new node:
+        nodeMalloc[i] = (Node){ids[i], i, curr, NULL, NULL, NULL, NULL};
         Node *this_node = &nodeMalloc[i];
-        this_node->v = ids[i];
-        this_node->l = curr; 
-        this_node->r = NULL;
-        this_node->prev_occ = NULL;
-        this_node->next_occ = NULL;
-        this_node->pair = NULL;
-        this_node->index = i;
         if (curr != NULL) {
             // link previous node:
             curr->r = this_node;
             int a = ids[i-1];
             int b = ids[i];
-            // add occurrence (only if not overlapping):
-            if (!(a == prev_a && b == prev_b && prev_added == i-1)){
-                prev_a = a;
-                prev_b = b;
-                prev_added = i;
-                int index = a*init_tokens+b;
-                // link previous/next occurrence of this pair:
-                Pair *curr_pair = &init_counts[index];
-                addOcc2Pair(curr_pair, curr);
-            }
+            int index = a*init_tokens+b;
+            // link previous/next occurrence of this pair:
+            Pair *curr_pair = &init_counts[index];
+            addOcc2Pair(curr_pair, curr);
         }
         curr = this_node;
     }
@@ -291,7 +325,6 @@ Token* train(int *ids, int n, int k, int init_tokens) {
             max_occ = this_occs;
     }
     max_occ++;
-    printf("max occ: %d\n", max_occ);
     
     // init heap:
     Pair **heap = malloc(sizeof(Pair*)*(max_occ));
@@ -305,20 +338,9 @@ Token* train(int *ids, int n, int k, int init_tokens) {
         addToHeap(heap, this_pair);
     }
     printL(&l);
-    for (int i=0; i<init_tokens; i++){
-        for (int j=0; j<init_tokens; j++){
-            int index = i*init_tokens+j;
-            printPair(&init_counts[index]);
-        }
-    }
-    printHeap(heap, max_occ);
-
     HashEntry *hashes = malloc(sizeof(HashEntry)*(init_tokens+k+1));
     for (int i=0; i<init_tokens+k+1; i++){
-        hashes[i].prev_connector_l = NULL;
-        hashes[i].prev_connector_r = NULL;
-        hashes[i].ref_l = NULL;
-        hashes[i].ref_r = NULL;
+        hashes[i] = (HashEntry) {-1, -1, NULL, NULL};
     }
 
     for (int i=0; i<k; i++){
@@ -326,79 +348,84 @@ Token* train(int *ids, int n, int k, int init_tokens) {
         while (heap_max > 0 && heap[heap_max] == NULL){
             heap_max--;
         }
-        if (heap_max < 0){
-            printf("Empty\n");
+        if (heap_max <= 0){
+            printf("Empty!!!:(\n");
             break;
         }
         Pair *max_pair = heap[heap_max];
         int new_letter = init_tokens+i;
-        printf("(%d, %d) -> {%d}", max_pair->a, max_pair->b, new_letter);
+        printf("(%d, %d) -> {%d}\n", max_pair->a, max_pair->b, new_letter);
 
         // delete each occurrence:
         Node *curr = max_pair->first_occurrence;
-        // for avoiding overlapping counts:
-        int prev_l = -1;
-        int did_add = 0;
+        // remove from heap
+        removeFromHeap(heap, max_pair);
         while (curr != NULL) {
+            Node *next_occ = curr->next_occ;
+            removeOccFromPair(max_pair, curr); // maybe inefficient that way
+            if (next_occ != NULL && next_occ->l == curr){
+                Node *next_next_occ = next_occ->next_occ;
+                removeOccFromPair(max_pair, next_occ);
+                next_occ = next_next_occ; // skip next occurrence
+            }
             // skip occurrence:
             removeOcc(heap, curr->l);
             removeOcc(heap, curr->r);
             // skip in doubly linked list:
-            if (curr->l != NULL){
-                curr->l->r = curr->r;
-            }
-            else {
-                l.start = curr; // make first if necessary: 
-            }
-            if (curr->r != NULL)
-                curr->r->l = curr->l;
+            removeFromList(&l, curr->r);
             curr->v = new_letter;
-            
             // add new occurrences
             if (curr->l != NULL){
-                if (!(prev_l == new_letter && curr->l == new_letter && did_add)) { //avoid overlaps
-                    Pair *p = getPairFromHash(hashes, curr->l, curr->v, new_letter);
-                    prev_l = curr->l;
-                    addToHeap(heap, p);
-                    addOcc(heap, p, curr->l);
-                    did_add = 1;
-                } else {
-                    did_add = 0;
-                }
+                Pair *p = getPairFromHash(heap, hashes, curr->l->v, new_letter, new_letter);
+                addOcc(heap, p, curr->l);
             }
             if (curr->r != NULL){
-                Pair *p = getPairFromHash(hashes, curr->v, curr->r, new_letter);
-                addToHeap(heap, p);
+                Pair *p = getPairFromHash(heap, hashes, new_letter, curr->r->v, new_letter);
                 addOcc(heap, p, curr);
             }
-            curr = curr->next_occ;
+            curr = next_occ;
         }
     }
-    free(heap);
-    free(nodeMalloc);
+    // printL(&l);
+    // printHeap(heap, max_occ);
+    
     free(init_counts);
+    // free remaining pairs:
+    while (heap_max >= 0){
+        if (heap[heap_max] != NULL){
+            Pair *curr = heap[heap_max];
+            while (curr != NULL){
+                Pair *next = curr->next;
+                if (!(curr->a < init_tokens && curr->b < init_tokens)){
+                    free(curr); 
+                }
+                curr = next;
+            }
+        }
+        heap_max--;
+    }
+    free(hashes);
+    free(nodeMalloc);
     return NULL;
 }
 
 int main() {
     srand(time(NULL));
-
     // Generate random array
-    int num_elements = 10;
-    int init_tokens = 5;
-    int* ids = malloc(sizeof(int) * num_elements);
+    int n = 8;
+    int k = 2;
+    int init_tokens = 3;
+    int* ids = malloc(sizeof(int) * n);
     if (!ids) {
         perror("Failed to allocate memory");
         return 1;
     }
-
-    for (int i = 0; i < num_elements; ++i) {
+    for (int i = 0; i < n; ++i) {
         ids[i] = rand() % init_tokens;
     }
-
-    int num_tokens = 10000;
-    Token* vocab = train(ids, num_elements, num_tokens, init_tokens);
-
+    //int test_ids[8] = {1, 1, 1, 1, 1, 1, 1, 1};
+    //memcpy(ids, test_ids, sizeof(test_ids));
+    train(ids, n, k, init_tokens);
     free(ids);
     return 0;
 }
